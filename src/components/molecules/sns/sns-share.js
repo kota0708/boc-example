a0ad4ca179ebdc11e91d33f4419fefa108c9e40c
kota <kota@imgsrc.co.jp>
2019-05-14 11:03:08 +0900

    update; shareボタンを作成

---
+++ src/components/molecules/sns/sns-share.js
@@ -0,0 +1,15 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  width: 100%;
+`;
+
+const Share = () => (
+  <Wrap>
+    <p>shearボタン</p>
+  </Wrap>
+);
+
+export default Share;
